ef9216257eb08ca2d658498a6458f1d7fa79826e
Josh Hill <josh.hill@makers.tech>
2019-09-11 09:52:00 +0100

    Comment route setup in app.js

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// route setup
 app.use('/', homeRouter);
 app.use('/posts', postsRouter);
 
