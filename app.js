b791c11ec5782e026dd51d450c637dc96fb8e7d5
John Forster <johnforster@makers.tech>
2022-03-11 18:46:58 +0000

    Update versions and old js

--- app.js
+++ app.js
@@ -1,78 +1,80 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var methodOverride = require('method-override')
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const methodOverride = require("method-override");
 
-var homeRouter = require('./routes/home');
-var postsRouter = require('./routes/posts');
-var sessionsRouter = require('./routes/sessions');
-var usersRouter = require('./routes/users');
+const homeRouter = require("./routes/home");
+const postsRouter = require("./routes/posts");
+const sessionsRouter = require("./routes/sessions");
+const usersRouter = require("./routes/users");
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"));
 
-app.use(session({ key: 'user_sid',
-secret: 'super_secret',
-resave: false,
-saveUninitialized: false,
-cookie: {
-  expires: 600000 }
-}));
+app.use(
+  session({
+    key: "user_sid",
+    secret: "super_secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 600000,
+    },
+  })
+);
 
 // clear the cookies after user logs out
 app.use((req, res, next) => {
   if (req.cookies.user_sid && !req.session.user) {
-    res.clearCookie('user_sid');
+    res.clearCookie("user_sid");
   }
   next();
 });
 
 // middleware function to check for logged-in users
-var sessionChecker = (req, res, next) => {
+const sessionChecker = (req, res, next) => {
   if (!req.session.user && !req.cookies.user_sid) {
-    res.redirect('/sessions/new');
+    res.redirect("/sessions/new");
   } else {
     next();
   }
 };
 
-
-
 // route setup
-app.use('/', homeRouter);
-app.use('/posts', sessionChecker, postsRouter);
-app.use('/sessions', sessionsRouter);
-app.use('/users', usersRouter);
+app.use("/", homeRouter);
+app.use("/posts", sessionChecker, postsRouter);
+app.use("/sessions", sessionsRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
