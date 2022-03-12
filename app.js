94e8f6c0a07512374e621f070cbba995a02ffda8
John Forster <johnforster@makers.tech>
2022-03-12 12:53:24 +0000

    Remove body-parser

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const bodyParser = require("body-parser");
 const session = require("express-session");
 const methodOverride = require("method-override");
 
@@ -20,10 +19,9 @@ app.set("view engine", "hbs");
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
 app.use(
