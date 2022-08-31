89eabad9f2d3a1df13951f5675ffb9d6771cd251
Eddie Andress <eddie_andress@hotmail.com>
2022-08-31 11:11:51 +0100

    rm routes not needed by API

--- routes/posts.js
+++ routes/posts.js
@@ -5,6 +5,5 @@ const PostsController = require("../controllers/posts");
 
 router.get("/", PostsController.Index);
 router.post("/", PostsController.Create);
-router.get("/new", PostsController.New);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const UsersController = require("../controllers/users");
 
-router.get("/new", UsersController.New);
 router.post("/", UsersController.Create);
 
 module.exports = router;
