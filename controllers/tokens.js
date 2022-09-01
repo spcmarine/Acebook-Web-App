f6d39f34401f624adca98aa6bc1b3a9f166c6610
Eddie Andress <eddie_andress@hotmail.com>
2022-09-01 17:58:31 +0100

    fixed flickering t & added logging

--- controllers/tokens.js
+++ controllers/tokens.js
@@ -9,12 +9,14 @@ const SessionsController = {
 
     User.findOne({ email: email }).then(async (user) => {
       if (!user) {
+        console.log("auth error: user not found")
         res.status(401).json({ message: "auth error" });
       } else if (user.password != password) {
+        console.log("auth error: passwords do not match")
         res.status(401).json({ message: "auth error" });
       } else {
         const token = await TokenGenerator.jsonwebtoken(user.id)
-        res.status(201).json({token: token, message: "OK"});
+        res.status(201).json({ token: token, message: "OK" });
       }
     });
   }
--- spec/controllers/tokens.spec.js
+++ spec/controllers/tokens.spec.js
@@ -4,9 +4,9 @@ require("../mongodb_helper");
 const User = require('../../models/user');
 
 describe("/tokens", () => {
-  beforeAll( async () => {
+  beforeAll( () => {
     const user = new User({ email: "test@test.com", password: "12345678" })
-    await user.save()
+    user.save()
   });
 
   afterAll( async () => {
@@ -17,8 +17,8 @@ describe("/tokens", () => {
     let response = await request(app)
       .post("/tokens")
       .send({email: "test@test.com", password: "12345678"})
-    expect(response.body.token).not.toEqual(undefined)
     expect(response.status).toEqual(201)
+    expect(response.body.token).not.toEqual(undefined)
     expect(response.body.message).toEqual("OK")
   })
 
@@ -27,8 +27,8 @@ describe("/tokens", () => {
     let response = await request(app)
       .post("/tokens")
       .send({email: "test@test.com", password: "1234"})
-    expect(response.body.token).toEqual(undefined)
     expect(response.status).toEqual(401)
+    expect(response.body.token).toEqual(undefined)
     expect(response.body.message).toEqual("auth error")
   })
 })
