ae3280ddca2191f269d36872b57e7b099bc9f05a
John Forster <john.forster@makers.tech>
2023-01-09 17:33:52 +0000

    Update Feed.cy.js
    Move mount below the intercept to prevent testing bug.

--- frontend/src/components/feed/Feed.cy.js
+++ frontend/src/components/feed/Feed.cy.js
@@ -4,7 +4,6 @@ const navigate = () => {}
 describe("Feed", () => {
   it("Calls the /posts endpoint and lists all the posts", () => {
     window.localStorage.setItem("token", "fakeToken")
-    cy.mount(<Feed navigate={navigate}/>)
     
     cy.intercept('GET', '/posts', (req) => {
         req.reply({
@@ -16,6 +15,8 @@ describe("Feed", () => {
         })
       }
     ).as("getPosts")
+
+    cy.mount(<Feed navigate={navigate}/>)
     
     cy.wait("@getPosts").then(() =>{
       cy.get('[data-cy="post"]')
