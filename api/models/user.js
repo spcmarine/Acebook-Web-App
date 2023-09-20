4060e7c219e50a3f366c5a3b5d0241eb86f40d7d
Dan Gibson <spcmarine@btinternet.com>
2023-09-20 16:31:57 +0100

    latest updates on backend

--- api/models/user.js
+++ api/models/user.js
@@ -3,6 +3,11 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
+  name: { type: String, required: true },
+  profilePic: {
+      data: Buffer,
+      contentType: String,
+  }
 });
 
 const User = mongoose.model("User", UserSchema);
