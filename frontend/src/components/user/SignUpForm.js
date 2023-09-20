9d81627c01a27553fc6d1fe74fda3368ca08fa11
Claudia_Alves <claudiascarvalves@gmail.com>
2023-09-20 15:00:05 +0100

    name and profile picture frontend formData added

--- frontend/src/components/user/SignUpForm.js
+++ frontend/src/components/user/SignUpForm.js
@@ -4,17 +4,24 @@ const SignUpForm = ({ navigate }) => {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [profilePic, setProfilePic] = useState(null);
+
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
+    const formData = new FormData();
+
+      formData.append("email", email);
+      formData.append("password", password);
+      formData.append("name", name);
+      formData.append("profilePic", profilePic);
+
+
     fetch( '/users', {
       method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email: email, password: password })
-    })
+      body: formData})
       .then(response => {
         if(response.status === 201) {
           navigate('/login')
@@ -32,11 +39,23 @@ const SignUpForm = ({ navigate }) => {
     setPassword(event.target.value)
   }
 
+  const handleNameChange = (event) => {
+    setName(event.target.value)
+  }
+
+  const handleProfilePicChange = (event) => {
+    const file = event.target.files[0];
+    setProfilePic(file);
+  }
+
 
     return (
       <form onSubmit={handleSubmit}>
           <input placeholder="Email" id="email" type='text' value={ email } onChange={handleEmailChange} />
           <input placeholder="Password" id="password" type='password' value={ password } onChange={handlePasswordChange} />
+          <input placeholder="Name" id="name" type='text' value={ name } onChange={handleNameChange} />
+          <input type='file' accept="image/*" id="ProfilePic" onChange={handleProfilePicChange} />
+
         <input id='submit' type="submit" value="Submit" />
       </form>
     );
