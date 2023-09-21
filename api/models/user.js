66eec146358a88aef45e020f762fcca446787b1e
Claudia_Alves <claudiascarvalves@gmail.com>
2023-09-21 10:52:13 +0100

    Proper commit now

--- api/models/user.js
+++ api/models/user.js
@@ -3,11 +3,8 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
-  name: { type: String, required: true },
-  profilePic: {
-      data: Buffer,
-      contentType: String,
-  }
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
 });
 
 const User = mongoose.model("User", UserSchema);
--- frontend/src/components/user/SignUpForm.js
+++ frontend/src/components/user/SignUpForm.js
@@ -4,24 +4,20 @@ const SignUpForm = ({ navigate }) => {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [name, setName] = useState("");
-  const [profilePic, setProfilePic] = useState(null);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const formData = new FormData();
-
-      formData.append("email", email);
-      formData.append("password", password);
-      formData.append("name", name);
-      formData.append("profilePic", profilePic);
-
-
     fetch( '/users', {
       method: 'post',
-      body: formData})
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email: email, password: password, firstName: firstName, lastName: lastName })
+    })
       .then(response => {
         if(response.status === 201) {
           navigate('/login')
@@ -39,13 +35,12 @@ const SignUpForm = ({ navigate }) => {
     setPassword(event.target.value)
   }
 
-  const handleNameChange = (event) => {
-    setName(event.target.value)
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value)
   }
 
-  const handleProfilePicChange = (event) => {
-    const file = event.target.files[0];
-    setProfilePic(file);
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value)
   }
 
 
@@ -53,9 +48,8 @@ const SignUpForm = ({ navigate }) => {
       <form onSubmit={handleSubmit}>
           <input placeholder="Email" id="email" type='text' value={ email } onChange={handleEmailChange} />
           <input placeholder="Password" id="password" type='password' value={ password } onChange={handlePasswordChange} />
-          <input placeholder="Name" id="name" type='text' value={ name } onChange={handleNameChange} />
-          <input type='file' accept="image/*" id="ProfilePic" onChange={handleProfilePicChange} />
-
+          <input placeholder="Name" id="name" type='text' value={ firstName } onChange={handleFirstNameChange} />
+          <input placeholder="Name" id="name" type='text' value={ lastName } onChange={handleLastNameChange} />
         <input id='submit' type="submit" value="Submit" />
       </form>
     );
