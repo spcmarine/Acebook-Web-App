b0825f34bf2a6d484c311a4d23362519f32915f0
elliepriestley <ellie.priestley@gmail.com>
2023-09-25 16:58:53 +0100

    Finished styling - updated headings, favicon, website title, posts page overall layout

--- frontend/src/components/auth/LoginForm.js
+++ frontend/src/components/auth/LoginForm.js
@@ -41,10 +41,12 @@ const LogInForm = ({ navigate }) => {
   <Navbar currentPage="login" />{
     <div className="d-flex flex-column justify-content-center align-items-center">
       <form onSubmit={handleSubmit} className="d-inline-flex flex-column justify-content-center align-items-center">
-        <h3 className="mb-5">Log In</h3>
+        <h3 className="d-flex justify-content-center ml-5 text-primary display-3 p-3">Log In</h3>
         <input placeholder='Email' id="email" className="d-inline-flex form-control" type='text' value={ email } onChange={handleEmailChange} />
         <input placeholder='Password' id="password" className=" d-inline-flex form-control" type='password' value={ password } onChange={handlePasswordChange} />
-        <input role='submit-button' className="btn btn-primary" id='submit' type="submit" value="Submit" />
+        <div className="d-flex justify-content-end p-3">
+          <input role='submit-button' className="btn btn-primary" id='submit' type="submit" value="Submit" />
+        </div>
       </form>
       </div>
       
